28eafcb824b056dccb55f4ffcef52b4215b6541f
Manuel Rodriguez <greluma2@gmail.com>
2024-03-11 16:43:30 +0100

    feat: Error 404 Component

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,14 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import WrapperInfo from "../components/WrapperInfo";
+import not_found from "../assets/images/not_found.svg";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
 
+  function handleNavigation() {
+    navigate("/");
+  }
+
   return (
-    <div>
-      <h2>Error Type</h2>
-      <h3>back home</h3>
-      <button onClick={() => navigate("/")}>Back Home</button>
-    </div>
+    <WrapperInfo
+      func={handleNavigation}
+      img={not_found}
+      title="not found"
+      btnText="back home"
+    />
   );
 };
 export default ErrorPage;
