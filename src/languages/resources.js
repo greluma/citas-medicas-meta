25dcbdebd02aae5eea0698c338e004b14cbb0e6b
Manuel Rodriguez <greluma2@gmail.com>
2024-03-12 00:26:26 +0100

    fix: login page design

--- src/languages/resources.js
+++ src/languages/resources.js
@@ -14,7 +14,7 @@ export const resources = {
       loginPage: {
         title: " technical test ",
         desc: " For the Front-End position at Meta-Enlace. Developed with React, TypeScript, SASS, Jest, react-router, Auth0 and more, I hope you like it.",
-        lgBtn: "log in or register",
+        lgBtn: "start",
       },
     },
   },
@@ -33,7 +33,7 @@ export const resources = {
       loginPage: {
         title: "prueba técnica ",
         desc: " Para el puesto de front-end en meta-enlace. Desarrollada con React, TypeScript, SASS, Jest, react-router, Auth0 y más, espero les guste.",
-        lgBtn: "iniciar sesión o registrarse",
+        lgBtn: "iniciar",
       },
     },
   },
