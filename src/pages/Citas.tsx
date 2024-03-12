14f4150727384a75cbd458c017af7d32dff1a131
Manuel Rodriguez <greluma2@gmail.com>
2024-03-12 20:26:37 +0100

    feat: Citas Page Design

--- src/pages/Citas.tsx
+++ src/pages/Citas.tsx
@@ -3,6 +3,7 @@ import PageTitle from "../components/PageTitle";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { useEffect } from "react";
 import { fetchDoctors } from "../features/appSlice";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const Citas = () => {
   const { t } = useTranslation();
@@ -13,26 +14,35 @@ const Citas = () => {
     dispatch(fetchDoctors());
   }, [dispatch]);
 
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("cita solicitada");
+  }
+
   return (
     <div className="content-wrapper">
       <PageTitle title={t("citas")} />
-      <form>
+      <form className="citas-form" onSubmit={handleSubmit}>
         <h3>solicitar nueva cita</h3>
-        <select name="especialistas" id="especialistas">
-          {doctors.map((doc) => (
-            <option key={doc.id} value={doc.id}>
-              {doc.name} - {doc.especialidad}
-            </option>
-          ))}
-        </select>
-        <button type="submit">+</button>
+        <div className="select-wrapper">
+          <select name="especialistas" id="especialistas">
+            {doctors.map((doc) => (
+              <option key={doc.id} value={doc.id}>
+                {doc.especialidad}
+              </option>
+            ))}
+          </select>
+          <button type="submit">
+            <IoIosAddCircleOutline />
+          </button>
+        </div>
       </form>
-      <div>
-        <h3>resumen de citas</h3>
+      <div className="citas-resumen">
         <ul>
           <li>cita 1</li>
           <li>cita 2</li>
           <li>cita 3</li>
+          <li>cita 1</li>
         </ul>
       </div>
     </div>
